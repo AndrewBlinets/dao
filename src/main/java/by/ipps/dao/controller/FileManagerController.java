4a81772652a87b58fdf1a79787f5b8cae20c92d7
Blinec_A <blinec_a@ipps.by>
2020-01-16 16:08:26 +0300

    loger filesave

--- src/main/java/by/ipps/dao/controller/FileManagerController.java
+++ src/main/java/by/ipps/dao/controller/FileManagerController.java
@@ -1,7 +1,10 @@
 package by.ipps.dao.controller;
 
 import by.ipps.dao.entity.FileManager;
+import by.ipps.dao.entity.Logger;
+import by.ipps.dao.entity.UserPortal;
 import by.ipps.dao.service.FileManagerService;
+import by.ipps.dao.service.LoggerService;
 import by.ipps.dao.utils.view.ViewFile;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.http.HttpStatus;
@@ -13,15 +16,22 @@ import org.springframework.web.bind.annotation.*;
 public class FileManagerController {
 
   private FileManagerService fileManagerService;
+  private LoggerService loggerService;
 
-  public FileManagerController(FileManagerService fileManagerService) {
+  public FileManagerController(FileManagerService fileManagerService,LoggerService loggerService) {
     this.fileManagerService = fileManagerService;
+    this.loggerService = loggerService;
   }
 
   @PostMapping
   @ResponseBody
-  public FileManager saveImage(@RequestBody FileManager fileManagers) {
-    return fileManagerService.save(fileManagers);
+  public FileManager saveImage(
+      @RequestBody FileManager fileManagers, @RequestParam(value = "user") UserPortal userPortal) {
+    fileManagers = fileManagerService.save(fileManagers);
+    if(fileManagers != null){
+      loggerService.create(new Logger(userPortal, String.valueOf(fileManagers.getClass()), fileManagers.getId(), "CREATE"));
+    }
+    return fileManagers;
   }
 
   @JsonView(ViewFile.BaseClass.class)
