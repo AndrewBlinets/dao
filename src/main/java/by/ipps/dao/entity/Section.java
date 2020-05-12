0943da498008d608aa18b10089df85e7af0336d2
Blinec_A <blinec_a@ipps.by>
2020-05-12 18:15:32 +0300

    refactor

--- src/main/java/by/ipps/dao/entity/Section.java
+++ src/main/java/by/ipps/dao/entity/Section.java
@@ -17,60 +17,60 @@ import java.util.List;
 @Entity
 @Data
 @FilterDef(
-    name = FilterName.STATUS,
-    defaultCondition = "status = :status and statusR = 'A'",
-    parameters = {
-      @ParamDef(name = "status", type = "integer"),
-    })
+        name = FilterName.STATUS,
+        defaultCondition = "status = :status and statusR = 'A'",
+        parameters = {
+                @ParamDef(name = "status", type = "integer"),
+        })
 public class Section extends BaseEntity implements Serializable {
-  @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
-  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-  @Filter(name = FilterName.LANGUAGE)
-  private List<SectionLanguageVersion> languageVersions;
+    @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Filter(name = FilterName.LANGUAGE)
+    private List<SectionLanguageVersion> languageVersions;
 
-  @Column
-  @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
-  private int type;
+    @Column
+    @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
+    private int type;
 
-  @Column
-  @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
-  private int index;
+    @Column
+    @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
+    private int index;
 
-  @Column
-  @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
-  private int status;
+    @Column
+    @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
+    private int status;
 
-  @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
-  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-  @OrderBy("index asc")
-  @Filter(name = FilterName.STATUS)
-  private List<Block> blocks;
+    @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OrderBy("index asc")
+    @Filter(name = FilterName.STATUS)
+    private List<Block> blocks;
 
-  @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
-  @OneToMany(fetch = FetchType.LAZY)
-  private List<FileManager> files;
+    @JsonView({ViewPage.AdminClass.class, ViewPage.SectionClass.class})
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<FileManager> files;
 
-  @Override
-  public String toString() {
-    final StringBuilder sb = new StringBuilder("{");
-    sb.append(super.toString());
-    sb.append(", \"languageVersions\" :[");
-    if (languageVersions != null)
-      for (SectionLanguageVersion languageVersion : languageVersions) {
-        sb.append("{\"id\" : ").append(languageVersion.getId()).append("},");
-      }
-    if (files != null)
-      for (FileManager fileManager : files) {
-        sb.append("{\"id\" : ").append(fileManager.getId()).append("},");
-      }
-    if (blocks != null)
-      for (Block block : blocks) {
-        sb.append("{\"id\" : ").append(block.getId()).append("},");
-      }
-    sb.append(", \"index\" :").append(index);
-    sb.append(", \"type\" :").append(type);
-    sb.append(", \"status\" :").append(status);
-    sb.append('}');
-    return sb.toString();
-  }
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("{");
+        sb.append(super.toString());
+        sb.append(", \"languageVersions\" :[");
+        if (languageVersions != null)
+            for (SectionLanguageVersion languageVersion : languageVersions) {
+                sb.append("{\"id\" : ").append(languageVersion.getId()).append("},");
+            }
+        if (files != null)
+            for (FileManager fileManager : files) {
+                sb.append("{\"id\" : ").append(fileManager.getId()).append("},");
+            }
+        if (blocks != null)
+            for (Block block : blocks) {
+                sb.append("{\"id\" : ").append(block.getId()).append("},");
+            }
+        sb.append(", \"index\" :").append(index);
+        sb.append(", \"type\" :").append(type);
+        sb.append(", \"status\" :").append(status);
+        sb.append('}');
+        return sb.toString();
+    }
 }
